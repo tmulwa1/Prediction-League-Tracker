4552df82c700c6b4bd021d8143851ac2dd872091
Tiffany Mulwa <tmulwa01@gmail.com>
2026-08-06 16:30:49 +0300

    Added a prediction function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
-from app.models import User
+from app.models import User, Event, Prediction
 from app import db
+from datetime import datetime
 
 # Blueprint for the main routes of the application
 main = Blueprint('main', __name__)
@@ -32,3 +33,46 @@ def login():
         return redirect(url_for('main.index'))
 
     return render_template('login.html')
+
+@main.route('/predict/<int:event_id>', methods=['GET', 'POST'])
+def predict(event_id):
+    user = get_current_user()
+    # If user doesn't exist, redirect to login page
+    if not user:
+        return redirect(url_for('main.login'))
+
+    event = Event.query.get(event_id)
+    if not event:
+        return "Event not found", 404
+
+    if event.lock_time <= datetime.utcnow():
+        return "Predictions are closed for this event", 403
+
+    if request.method == 'POST':
+        # F1 branch
+        if event.sport == 'F1':
+            predicted_winner = request.form.get('predicted_winner')
+            predicted_podium = request.form.get('predicted_podium')
+
+            prediction = Prediction(
+                user_id=user.id,
+                event_id=event_id,
+                predicted_winner=predicted_winner,
+                predicted_podium=predicted_podium
+            )
+        # Football branch
+        else:
+            predicted_home_score = int(request.form.get('predicted_home_score'))
+            predicted_away_score = int(request.form.get('predicted_away_score'))
+
+            prediction = Prediction(
+                user_id=user.id,
+                event_id=event_id,
+                predicted_home_score=predicted_home_score,
+                predicted_away_score=predicted_away_score
+            )
+        db.session.add(prediction)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    
+    return render_template('predict.html')
